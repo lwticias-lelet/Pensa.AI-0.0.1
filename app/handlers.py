9a9dcddea00399df77d09489456a2168acc4231a
Letícia Delfino <149633758+lwticias-lelet@users.noreply.github.com>
2025-06-14 22:56:37 -0300

    Delete app directory

--- app/handlers.py
+++
@@ -1,6 +0,0 @@
-from backend.openrouter_client import call_model
-from backend.prompts import wrap_prompt
-
-def process_input(question):
-    prompt = wrap_prompt(question)
-    return call_model(prompt)
